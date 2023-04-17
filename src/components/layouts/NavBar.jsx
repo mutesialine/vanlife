4896b97e47a3235977e7fd8eefa7ef77839060da
Mutesi <ktesialine@gmail.com>
2023-04-17 15:31:13 +0200

    feat:vanlife navbar

---
+++ src/components/layouts/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <div className="py-8  px-12 flex justify-between items-center">
+      <div className="text-4xl font-bold">#VANLIFE</div>
+      <div className="flex gap-4 text-lg">
+        <p>About</p>
+        <p>Van</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/layouts/NavBar";
+
+const Home = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
