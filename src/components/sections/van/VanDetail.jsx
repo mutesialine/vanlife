75e6886bd1dd27c50f648f81aa27ebfec2c884d1
Mutesi <ktesialine@gmail.com>
2023-04-19 17:58:01 +0200

    fix:mobile responsiveness

--- src/components/sections/van/VanDetail.jsx
+++ src/components/sections/van/VanDetail.jsx
@@ -12,19 +12,20 @@ const VanDetail = () => {
   return (
     <div className="max-w-7xl mx-auto gap-8 py-16 px-24">
       {van ? (
-        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+        <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
           <img src={van.imageUrl} alt="element" className="w-[500px]" />
           <div className="space-y-8">
-            <div className="flex items-center gap-x-4 justify-between">
+            <div className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-8 justify-between">
               <h2 className="text-3xl font-bold">{van.name}</h2>
-              <p className=" flex flex-col md:flex-row text-xl font-semibold">
+              <p className=" flex  text-xl font-semibold">
                 ${van.price}
                 <span>/day</span>
               </p>
             </div>
             <h2 className="text-2xl font-semibold">{van.description}</h2>
-            <button className="px-8 py-2 rounded-md bg-orange-500 w-fit">
-              {van.type}
+            <p className="text-orange-500">{van.type}</p>
+            <button className="px-8 py-2 rounded-md bg-orange-500 w-full md:w-fit text-white">
+              Rent this van
             </button>
           </div>
         </div>
--- src/components/ui/VanCard.jsx
+++ src/components/ui/VanCard.jsx
@@ -4,14 +4,14 @@ const VanCard = ({ imageUrl, name, price, type }) => {
   return (
     <div className="flex flex-col gap-y-3">
       <img src={imageUrl} alt="element" className="w-[500px]" />
-      <div className="flex items-center gap-x-16 ">
+      <div className="flex items-center gap-x-4 justify-between">
         <h2 className="text-3xl font-bold">{name}</h2>
         <p className=" flex flex-col text-xl font-semibold">
           ${price}
           <span>/day</span>
         </p>
       </div>
-      <button className="px-8 py-2 rounded-md bg-orange-500 w-fit">
+      <button className="px-8 py-2 rounded-md bg-orange-500 w-fit text-white">
         {type}
       </button>
     </div>
