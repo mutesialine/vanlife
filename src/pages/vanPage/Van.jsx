3f9efb97226fd99af8e3e69223bf3244ed5b9d06
Mutesi <ktesialine@gmail.com>
2023-04-23 20:01:07 +0200

    feat: add van title

--- src/pages/vanPage/Van.jsx
+++ src/pages/vanPage/Van.jsx
@@ -10,10 +10,15 @@ const Van = () => {
   }, []);
 
   return (
-    <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-8 py-20 px-6 md:px-24">
-      {vans.map((element) => (
-        <VanCard {...element} key={element.id} />
-      ))}
+    <div className="max-w-7xl mx-auto pt-16 pb-24 px-6 md:px-24">
+      <h2 className="text-3xl md:text-5xl font-bold pb-10">
+        Explore our Vans options
+      </h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+        {vans.map((element) => (
+          <VanCard {...element} key={element.id} />
+        ))}
+      </div>
     </div>
   );
 };
