f926b18a1d653c625079c3ea27bc7a670e562fb9
Mutesi <ktesialine@gmail.com>
2023-05-02 20:55:49 +0200

    feat:await component ,defer feature on host van details

--- src/pages/host/HostVanDetail.jsx
+++ src/pages/host/HostVanDetail.jsx
@@ -1,33 +1,41 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom";
+import { Await, Link, Outlet, defer, useLoaderData } from "react-router-dom";
 import { HostVanLayout } from "../../components/layouts";
 import { getHostVans } from "../../Api";
 import { requireAuth } from "../../Utils";
 
-export const loader = async ({ params }) => {
-  await requireAuth();
-  return getHostVans(params.id);
+export const loader = async ({ params, request }) => {
+  await requireAuth(request);
+  return defer({ currentVan: getHostVans(params.id) });
 };
 
 const HostVanDetail = () => {
-  const currentVan = useLoaderData();
+  const loadedVan = useLoaderData();
   return (
     <div className="max-w-7xl mx-auto pt-12 pb-24 px-6 md:px-24 space-y-4 mt-6 bg-white">
       <Link to="/host/vans" className="text-2xl text-center">
         &larr; <span className="text-lg hover:underline">Back to all vans</span>
       </Link>
-      <div className="flex flex-col md:flex-row md:items-center gap-x-12 gap-y-6">
-        <img src={currentVan.imageUrl} alt="element" className="w-[300px]" />
-        <div className="space-y-8 text-3xl">
-          <p className="bg-orange-500 py-2 px-28 w-fit text-white rounded-md text-2xl ">
-            {currentVan.type}
-          </p>
-          <h2 className="text-3xl font-bold">{currentVan.name}</h2>
-          <p className=" flex  text-xl font-semibold">
-            ${currentVan.price}
-            <span>/day</span>
-          </p>
-        </div>
-      </div>
+      <Await resolve={loadedVan.currentVan}>
+        {(currentVan) => (
+          <div className="flex flex-col md:flex-row md:items-center gap-x-12 gap-y-6">
+            <img
+              src={currentVan.imageUrl}
+              alt="element"
+              className="w-[300px]"
+            />
+            <div className="space-y-8 text-3xl">
+              <p className="bg-orange-500 py-2 px-28 w-fit text-white rounded-md text-2xl ">
+                {currentVan.type}
+              </p>
+              <h2 className="text-3xl font-bold">{currentVan.name}</h2>
+              <p className=" flex  text-xl font-semibold">
+                ${currentVan.price}
+                <span>/day</span>
+              </p>
+            </div>
+          </div>
+        )}
+      </Await>
 
       <HostVanLayout />
       <Outlet context={{ currentVan }} />
