d98eecdc2a4d5946816eb349e371174f8c8ba901
Mutesi <ktesialine@gmail.com>
2023-05-02 20:48:53 +0200

    feat: add await components  and defer feature on van details page

--- src/pages/van/VanDetail.jsx
+++ src/pages/van/VanDetail.jsx
@@ -1,13 +1,19 @@
-import { Link, useLocation, useLoaderData } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import { getVans } from "../../Api";
 
 export const loader = ({ params }) => {
-  return getVans(params.id);
+  return defer({ van: getVans(params.id) });
 };
 
 const VanDetail = () => {
   const location = useLocation();
-  const van = useLoaderData();
+  const loadedvanDetails = useLoaderData();
 
   const search = location.state?.search || "null";
   const type = location.state?.type || "All";
@@ -18,23 +24,27 @@ const VanDetail = () => {
         &larr;{" "}
         <span className="text-lg hover:underline">{`back to ${type} vans`}</span>
       </Link>
-      <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
-        <img src={van.imageUrl} alt="element" className="w-[500px]" />
-        <div className="space-y-8">
-          <div className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-8 justify-between">
-            <h2 className="text-3xl font-bold">{van.name}</h2>
-            <p className=" flex  text-xl font-semibold">
-              ${van.price}
-              <span>/day</span>
-            </p>
+      <Await resolve={loadedvanDetails.van}>
+        {(van) => (
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
+            <img src={van.imageUrl} alt="element" className="w-[500px]" />
+            <div className="space-y-8">
+              <div className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-8 justify-between">
+                <h2 className="text-3xl font-bold">{van.name}</h2>
+                <p className=" flex  text-xl font-semibold">
+                  ${van.price}
+                  <span>/day</span>
+                </p>
+              </div>
+              <h2 className="text-2xl font-semibold">{van.description}</h2>
+              <p className="text-orange-500">{van.type}</p>
+              <button className="px-8 py-2 rounded-md bg-orange-500 w-full md:w-fit text-white">
+                Rent this van
+              </button>
+            </div>
           </div>
-          <h2 className="text-2xl font-semibold">{van.description}</h2>
-          <p className="text-orange-500">{van.type}</p>
-          <button className="px-8 py-2 rounded-md bg-orange-500 w-full md:w-fit text-white">
-            Rent this van
-          </button>
-        </div>
-      </div>
+        )}
+      </Await>
     </div>
   );
 };
