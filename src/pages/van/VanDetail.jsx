fd8031eecd8132eaf2266eb94e1d86d64d058538
Mutesi <ktesialine@gmail.com>
2023-05-03 04:18:25 +0200

    feat:add sespense feature on van details and host van details page

--- src/pages/van/VanDetail.jsx
+++ src/pages/van/VanDetail.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import {
   Link,
   useLocation,
@@ -24,27 +25,29 @@ const VanDetail = () => {
         &larr;{" "}
         <span className="text-lg hover:underline">{`back to ${type} vans`}</span>
       </Link>
-      <Await resolve={loadedvanDetails.van}>
-        {(van) => (
-          <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
-            <img src={van.imageUrl} alt="element" className="w-[500px]" />
-            <div className="space-y-8">
-              <div className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-8 justify-between">
-                <h2 className="text-3xl font-bold">{van.name}</h2>
-                <p className=" flex  text-xl font-semibold">
-                  ${van.price}
-                  <span>/day</span>
-                </p>
+      <Suspense fallback={<h2>loading...</h2>}>
+        <Await resolve={loadedvanDetails.van}>
+          {(van) => (
+            <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
+              <img src={van.imageUrl} alt="element" className="w-[500px]" />
+              <div className="space-y-8">
+                <div className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-8 justify-between">
+                  <h2 className="text-3xl font-bold">{van.name}</h2>
+                  <p className=" flex  text-xl font-semibold">
+                    ${van.price}
+                    <span>/day</span>
+                  </p>
+                </div>
+                <h2 className="text-2xl font-semibold">{van.description}</h2>
+                <p className="text-orange-500">{van.type}</p>
+                <button className="px-8 py-2 rounded-md bg-orange-500 w-full md:w-fit text-white">
+                  Rent this van
+                </button>
               </div>
-              <h2 className="text-2xl font-semibold">{van.description}</h2>
-              <p className="text-orange-500">{van.type}</p>
-              <button className="px-8 py-2 rounded-md bg-orange-500 w-full md:w-fit text-white">
-                Rent this van
-              </button>
             </div>
-          </div>
-        )}
-      </Await>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 };
